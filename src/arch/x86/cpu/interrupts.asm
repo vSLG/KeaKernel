f908dd60ccce3dcbb8d1f1b49abf73e1458552d2
vslg <slgf@protonmail.ch>
2021-05-21 08:32:59 -0300

    x86_64: idt: fix wrong pop order of popall

--- src/arch/x86/cpu/interrupts.asm
+++ src/arch/x86/cpu/interrupts.asm
@@ -53,21 +53,21 @@ __int_%2:
 %endmacro
 
 %macro popall 0
-    pop     r15
-    pop     r14
-    pop     r13
-    pop     r12
-    pop     r11
-    pop     r10
-    pop     r9
-    pop     r8
-    pop     rbp
-    pop     rdi
-    pop     rsi
-    pop     rdx
-    pop     rcx
-    pop     rbx
     pop     rax
+    pop     rbx
+    pop     rcx
+    pop     rdx
+    pop     rsi
+    pop     rdi
+    pop     rbp
+    pop     r8
+    pop     r9
+    pop     r10
+    pop     r11
+    pop     r12
+    pop     r13
+    pop     r14
+    pop     r15
 %endmacro
 
 
